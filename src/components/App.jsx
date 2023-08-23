9e50f192789686d1dd90c9e2beba67b3d2286e7e
IT14KR <131267129+IT14KR@users.noreply.github.com>
2023-08-23 23:13:57 +0200

    add changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import { Notification } from './Notification/Notification';
 import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
